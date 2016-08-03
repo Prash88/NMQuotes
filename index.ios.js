80074648125d813234ecfb4f6166205e5fb4f321
Prashanth Sampath Kumar <pskumar@fb.com>
2016-08-02 20:49:30 -0700

    Adding initial views

--- index.ios.js
+++ index.ios.js
@@ -1,32 +1,71 @@
 /**
- * Sample React Native App
+ * NMQuotes
  * https://github.com/facebook/react-native
  * @flow
  */
+
+'use strict';
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TouchableOpacity,
 } from 'react-native';
+const NMSaid = require('./NMQuotes.json');
 
-class NMQuotes extends Component {
+const SIGNATURE_URL = 'https://upload.wikimedia.org/wikipedia/commons/e/ec/Signature_of_Narendra_Modi.svg';
+
+type State = {
+  quote: string,
+};
+
+type Props = {
+
+};
+
+class NMQuotes extends Component<void, Props, State> {
+
+  state: State;
+  props: Props;
+
+  static propTypes = {
+  };
+
+  constructor() {
+    super();
+    this.state = {
+      quote: NMSaid.ModiQuotes[
+        Math.floor(Math.random() * NMSaid.ModiQuotes.length)
+      ],
+    };
+  }
+
+  //Touch Function
+  _changeQuote() {
+     this.setState({
+      quote: NMSaid.ModiQuotes[
+        Math.floor(Math.random() * NMSaid.ModiQuotes.length)
+      ],
+    });
+  }
+
+  //Render Function
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <View
+        style={styles.container}
+        onTouchStart={this._changeQuote}>
+       <Text style={styles.quote}>
+           {this.state.quote}
+       </Text>
+       <Image
+        style={styles.signature}
+        source={{uri: SIGNATURE_URL}}/>
+     </View>
     );
   }
 }
@@ -36,17 +75,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'black',
   },
-  welcome: {
-    fontSize: 20,
+  quote: {
+    fontSize: 30,
     textAlign: 'center',
     margin: 10,
+    color: 'white',
+    fontFamily: 'Cochin'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  signature: {
+    height: 30,
+    width: 100
   },
 });
 
